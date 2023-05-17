0c142849b9fed632bab1f514ccb87f3ca7062e41
messiasjs <mjls.junior.js@gmail.com>
2023-05-17 18:07:04 -0300

    atualizando

--- P3/app.py
+++ P3/app.py
@@ -1,26 +1,34 @@
 from flask import Flask, render_template, make_response, request
 import requests, json
+from datetime import date
 
 app = Flask(__name__, template_folder='templates')
 url = 'https://script.google.com/macros/s/AKfycbxzW3QGiAbYIialGTzt2sAZkj7AzskLCABYf-gCmWL70S79Fh6pIZlHvILatc7qqAp6/exec'
+data_atual = date.today()
+
 
 
 @app.route('/')
 def index():
     year = request.args.get('ano')
-    month = request.args.get('mes')
-    #print("---------------------- ", request.headers)
-    #year = args['ano']
-    #month = args['mes']
-    #url = 'https://script.google.com/macros/s/AKfycbxzW3QGiAbYIialGTzt2sAZkj7AzskLCABYf-gCmWL70S79Fh6pIZlHvILatc7qqAp6/exec?mes=3&ano=2023&rota=getLivrosPorMes'
-    
-    if month and year:
-        params = '?mes='+month+'&ano='+year+'&rota=getLivrosPorMes'
-        
-        response = requests.get(url+params)
-        data = response.json()
 
-        resp =  make_response(render_template("index.html", data=data))
+    if year:
+        livro_mes = []
+        if int(year) < data_atual.year:
+            for month in range(1,13):
+                params = '?mes='+str(month)+'&ano='+year+'&rota=getGrafico'
+                response = requests.get(url+params)
+                data = response.json()
+                livro_mes.append(data)
+
+        else:
+            for month in range(1,data_atual.month):
+                params = '?mes='+str(month)+'&ano='+year+'&rota=getGrafico'
+                response = requests.get(url+params)
+                data = response.json()
+                livro_mes.append(data)
+        
+        resp =  make_response(render_template("index.html", data=livro_mes))
         return resp
     else :
         return render_template("index.html")
